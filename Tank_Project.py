7d342db5b854a47e3a99695b51dd5d4b9a671605
Extranjero13 <arivera0229@yahoo.com>
2024-02-14 21:17:51 -0500

    Final

--- Tank_Project.py
+++ Tank_Project.py
@@ -1,70 +1,89 @@
 import turtle
+import random
+import math
 
-# Screen Settings
+#Screen Settings
 screen = turtle.Screen()          # for screen (window)
 screen.setup(500, 500)
 screen.tracer(0)             # tell screen to not show automatically
 
-# Tank draw settings
+#Tank draw settings
 tank = turtle.Turtle()
 tank.speed(0)                 # draw as fast as possible
 tank.width(4)                    # line thickness
-tank.hideturtle()             # hide the turtle
-head= turtle.Turtle()
-head.speed(0)
-head.width(2)
-head.hideturtle()
-
-def draw_tank():
-    r=25
-    for side in range(4):    # draw a square using a for loop, 3 turns so range = 4. Side is a variable (i)
-        tank.forward(100)    #width of tank object (square)
-        tank.left(90)
-        for i in range(4):
-            head.circle(r)
-            head.goto(tank.xcor()-50,tank.ycor()-50)
-       
-    
+           # hide the turtle
 
 
-# Tank moving commands
+def draw_tank(tank):
+    tank.clear()
+    # Draw the line inside the circle
+    tank.dot(50,"green")
+    tank.forward(30)            #turret draw
+    tank.backward(30)
+    screen.update()
+
+x = 0
+y = 0
+angle = 0
+velocity = 5
+
+def movement(angle, velocity):
+
+    angle = math.radians(angle)
+
+    dx = velocity * math.cos(angle)
+    dy = velocity * math.sin(angle)
+
+    return dx, dy
+
+def move():
+    global x, y
+    dx, dy = movement(angle, velocity)
+
+    x += dx
+    y += dy
+
+    tank.goto(x, y)
+    draw_tank(tank)
+
+
 def down():
-    tank.setheading(270)
-    tank.forward(10)
-    head.setheading(270)
-    head.forward(10)
+    global angle
+    angle = 270
+    move()
+
 def up():
-    tank.setheading(90)
-    tank.forward(10)
-    head.setheading(90)
-    head.forward(10)
+    global angle
+    angle = 90
+    move()
+
 def left():
-    tank.setheading(180)
-    tank.forward(10)
-    head.setheading(180)
-    tank.forward(10)
+    global angle
+    angle = 180
+    tank.left(18)
+    move()
 
 def right():
-    tank.setheading(0)
-    tank.forward(10)
-    head.setheading(0)
-    head.forward(10)
+    global angle
+    angle = 0
+    tank.right(18)
+    move()
 
-turtle.listen()                 # Begin listening for arrow key commands
-turtle.onkey(down, "Down")      # Sets up instructions for keystrokes
-turtle.onkey(up, "Up") 
+
+
+
+turtle.listen()
+turtle.onkey(down, "Down")
+turtle.onkey(up, "Up")
 turtle.onkey(left, "Left")
 turtle.onkey(right, "Right")
 
-# Execute main loop/ tank moving
-while True:
-    tank.clear()
-    head.clear()
-    draw_tank()
-    screen.update()
 
+draw_tank(tank)
 
+screen.update()
 
-  
+turtle.mainloop()
+
 
 
